88ee159e94bc70b63ba74307790e694f69c12679
WelderBM <Welderb40@gmail.com>
2021-11-02 21:25:55 -0300

    added new questions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <h1>Desfio de Funções com JavaScript</h1>
 
-<p>Neste desafio faço uma compilação de exercícios para fixar o conhecimento adquirodo no Code Clube sobre funções. Até agora os desafio têm sido bem sucedidos sem intervenção do vídeo de correção, mas com os recursos que um programador se utiliza no dia a dia, com principal a posquisa para resolução de problemas encontrados. Continuo aprendendo bastante e em bs=usca de umaoportunidade.</p>
+<p>Neste desafio faço uma compilação de exercícios para fixar o conhecimento adquirodo no Code Clube sobre funções. Até agora os desafio têm sido bem sucedidos sem intervenção do vídeo de correção, mas com os recursos que um programador se utiliza no dia a dia, com principal a posquisa para resolução de problemas encontrados.</p>
--- scripts.js
+++ scripts.js
@@ -62,3 +62,81 @@ function Bigger(firstNumber, secondNumber) {
         }
     }
 }
+
+function arithmeticAverage() {
+    let wageOne = parseInt(document.getElementById("input-6-1").value)
+    let wageTwo = parseInt(document.getElementById("input-6-2").value)
+    let wageThree = parseInt(document.getElementById("input-6-3").value)
+    let wageFour = parseInt(document.getElementById("input-6-4").value)
+    let wageFive = parseInt(document.getElementById("input-6-5").value)
+
+    let x = ""
+
+    let firstChildnumber = 0
+    let secondChildnumber = 0
+    let thirdChildnumber = 0
+    let fourthChildnumber = 0
+    let fifthChildnumber = 0
+    console.log(firstChildnumber, secondChildnumber, thirdChildnumber, fourthChildnumber, fifthChildnumber);
+    if (parseInt((document.getElementById("input-6-6").value)) == NaN) {
+        firstChildnumber = 0
+    } else{ firstChildnumber = parseInt(document.getElementById("input-6-6").value)}
+    if (parseInt((document.getElementById("input-6-7").value)) != "NaN") {
+        secondChildnumber = parseInt(document.getElementById("input-6-7").value)
+    } else{secondChildnumber = 0}
+    if (parseInt((document.getElementById("input-6-8").value)) != "NaN") {
+        thirdChildnumber = parseInt(document.getElementById("input-6-8").value)
+    } else{thirdChildnumber = 0}
+    if (parseInt((document.getElementById("input-6-9").value)) != "NaN") {
+        fourthChildnumber = parseInt(document.getElementById("input-6-9").value)
+    }else{fourthChildnumber = 0}
+    if (parseInt((document.getElementById("input-6-10").value)) != "NaN") {
+        fifthChildnumber = parseInt(document.getElementById("input-6-10").value)
+    } else {fifthChildnumber = 0}
+    console.log(firstChildnumber, secondChildnumber, thirdChildnumber, fourthChildnumber, fifthChildnumber);
+
+    firstChildnumber = parseInt(document.getElementById("input-6-6").value)
+    firstChildnumber = parseInt(document.getElementById("input-6-6").value)
+    firstChildnumber = parseInt(document.getElementById("input-6-6").value)
+    firstChildnumber = parseInt(document.getElementById("input-6-6").value)
+    if ((wageOne > wageTwo & wageOne > wageThree && wageOne > wageFour && wageOne > wageFive) || (wageTwo > wageOne && wageTwo > wageThree && wageTwo > wageFour && wageTwo > wageFive) || (wageThree > wageTwo && wageThree > wageOne && wageThree > wageFour && wageThree > wageFive) || (wageFour > wageTwo && wageFour > wageThree && wageFour > wageOne && wageFour > wageFive) || (wageFive > wageTwo && wageFive > wageThree && wageFive > wageFour && wageFive > wageOne)) {
+        if (wageOne > wageTwo && wageOne > wageThree && wageOne > wageFour && wageOne > wageFive) {
+            x = `o salário de valor(${wageOne}) é o maior`
+            document.getElementById("answer-6").innerHTML = `o salário de valor(${wageOne}) é o maior`
+        }
+        if (wageTwo > wageOne && wageTwo > wageThree && wageTwo > wageFour && wageTwo > wageFive) {
+            x = `o salário de valor(${wageTwo}) é o maior`
+        }
+        if (wageThree > wageTwo && wageThree > wageOne && wageThree > wageFour && wageThree > wageFive) {
+            x = `o salário de valor(${wageThree}) é o maior`
+        }
+        if (wageFour > wageTwo && wageFour > wageThree && wageFour > wageOne && wageFour > wageFive) {
+            x = `o salário de valor(${wageFour}) é o maior`
+        }
+        if (wageFive > wageTwo && wageFive > wageThree && wageFive > wageFour && wageFive > wageOne) {
+            x = `o salário de valor(${wageFive}) é o maior`
+        }
+    }
+    if (wageOne === wageTwo || wageOne === wageThree || wageOne === wageFour || wageOne === wageFive || wageTwo === wageOne || wageTwo === wageThree || wageTwo === wageFour || wageTwo === wageFive || wageThree === wageTwo || wageThree === wageOne || wageThree === wageFour || wageThree === wageFive || wageFour === wageTwo || wageFour === wageThree || wageFour === wageOne || wageFour === wageFive || wageFive === wageTwo || wageFive === wageThree || wageFive === wageFour || wageFive === wageOne) {
+        return document.getElementById("answer-6").innerHTML = `Você colocou algum salário igual a outro`
+    } else if (x === "") {
+        return document.getElementById("answer-6").innerHTML = `você esqueceu de algum valor`
+    }
+    if (firstChildnumber === 0 || secondChildnumber === 0 || thirdChildnumber === 0 || fourthChildnumber === 0 || fifthChildnumber === 0) {
+        return document.getElementById("answer-6").innerHTML = `você esqueceu de algum valor`
+    }
+
+    x = x + `<p>A média entre os salários é: ${(wageOne + wageTwo + wageThree + wageFour + wageFive) / 5}</p>`
+
+    let y = ((firstChildnumber + secondChildnumber + thirdChildnumber + fourthChildnumber + fifthChildnumber) / 5)
+
+    if ((y === Math.ceil(y))) {
+        x = x + `<p>A média entre o número de filhos é ${Math.floor((firstChildnumber + secondChildnumber + thirdChildnumber + fourthChildnumber + fifthChildnumber) / 5)}</p>`
+    } else if ((y + 0.5 > Math.ceil(y))) {
+        x = x + `<p>A média entre o número de filhos é um pouco menor que ${Math.ceil((firstChildnumber + secondChildnumber + thirdChildnumber + fourthChildnumber + fifthChildnumber) / 5)}</p>`
+    } else {
+        x = x + `<p>A média entre o número de filhos é um pouco maior que ${Math.floor((firstChildnumber + secondChildnumber + thirdChildnumber + fourthChildnumber + fifthChildnumber) / 5)}</p>`
+    }
+
+    document.getElementById("answer-6").innerHTML = x
+}
